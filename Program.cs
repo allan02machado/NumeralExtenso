05a459ff9b6ec8e52c2eca8982d15f3d37d15abf
allan02machado <70041162+allan02machado@users.noreply.github.com>
2020-10-07 22:33:41 -0300

    Este commit adiciona resposta ao exercício

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Humanizer;
+
+namespace NumeralExtenso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.Write("Digite um número: ");
+            numero = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            string numeroPorExtenso = numero.ToWords();
+            Console.WriteLine($"O número {numero} em extenso fica: {numeroPorExtenso}");
+        }
+    }
+}
